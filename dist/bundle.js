e5e9e2d047af880311b57ba1f2a00ab9115ba19d
csrigogogo <cri.sri@outlook.com>
2022-06-09 17:41:59 +0800

    add webpack-dev-server

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("/*\r\n * @Author: csrigogogo cri.sri@outlook.com\r\n * @Date: 2022-06-09 16:59:14\r\n * @LastEditors: csrigogogo cri.sri@outlook.com\r\n * @LastEditTime: 2022-06-09 17:18:37\r\n * @FilePath: \\webpack-vue\\src\\main.js\r\n * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE\r\n */\r\n\r\nconsole.log(\"hello world es5\");\r\n\n\n//# sourceURL=webpack://webpack-vue/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/main.js"]();
+/******/ 	
+/******/ })()
+;
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@
  * @Author: csrigogogo cri.sri@outlook.com
  * @Date: 2022-06-09 16:59:14
  * @LastEditors: csrigogogo cri.sri@outlook.com
- * @LastEditTime: 2022-06-09 17:04:19
+ * @LastEditTime: 2022-06-09 17:18:37
  * @FilePath: \webpack-vue\src\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-console.log("hello world");
+console.log("hello world es5");
