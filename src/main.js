773e84a0c1728805f6c7923ad868f119307dd7ff
csrigogogo <cri.sri@outlook.com>
2022-06-09 18:06:24 +0800

    add vue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@
  * @Author: csrigogogo cri.sri@outlook.com
  * @Date: 2022-06-09 16:59:14
  * @LastEditors: csrigogogo cri.sri@outlook.com
- * @LastEditTime: 2022-06-09 17:53:49
+ * @LastEditTime: 2022-06-09 17:59:44
  * @FilePath: \webpack-vue\src\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-
+// 导入vue
+import Vue from '../node_modules/vue/index'
+var vm = new Vue({
+    el: '#app',
+    data: {
+        msg: 'Hello Vue!'
+    }
+});
 console.log("hello world es511");
